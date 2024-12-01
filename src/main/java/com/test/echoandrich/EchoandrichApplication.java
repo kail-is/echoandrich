49c5df6c8410eebf4d089d51021d58b1c0f8b5aa
jeansuh42 <jeansuh42@gmail.com>
2024-12-02 01:57:10 +0900

    Project Init

---
+++ src/main/java/com/test/echoandrich/EchoandrichApplication.java
@@ -0,0 +1,13 @@
+package com.test.echoandrich;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoandrichApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchoandrichApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/echoandrich/EchoandrichApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.echoandrich;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchoandrichApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
